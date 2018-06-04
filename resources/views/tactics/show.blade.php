974dfa0433032463a2b90e75bca44b1526ecd9cb
Hendrik Van Dijck <hendrik@pau.be>
2018-06-05 00:57:40 +0200

    update layout for canvas

--- resources/views/tactics/show.blade.php
+++ resources/views/tactics/show.blade.php
@@ -2,12 +2,13 @@
 @section('content')
   <div class="row">
     <div class="col-6">
-      <input type="number" name="step" id="step" value=1 onchange="updateStep(this.value)" min="1" max="{{$max}}">
-      <canvas id="soccerfield" height="105" width="68" style="background: blue; width:100%;"></canvas>
+      <input type="number" name="step" id="step" value=1 onchange="updateStep(this.value)" min="1" max="{{$max+1}}">
+      <button onclick="runSteps('{{$max}}')">Play!</button>
+      <button onclick="resetSteps()">Reset steps</button>
+      <canvas id="soccerfield" height="410" width="272" style="background: blue; width:100%;"></canvas>
     </div>
     <div class="col-6">
       {{$max}}
-        {{ $coordinates[0] }}
     </div>
   </div>
 
@@ -21,9 +22,12 @@
     @foreach($coordinates as $coordinate)
       if({{$coordinate->step}} == 1){
         ctx.beginPath();
-        ctx.arc({{$coordinate->xCoordinate}}, {{$coordinate->yCoordinate}}, 2, 0, 2 * Math.PI);
+        ctx.arc({{$coordinate->xCoordinate.', '.$coordinate->yCoordinate}}, 10, 0, 2 * Math.PI);
         ctx.fillStyle = "#000000";
         ctx.fill();
+        ctx.fillStyle = "#FFFFFF";
+        ctx.font="12px Arial";
+        ctx.fillText("{{$coordinate->id}}",{{$coordinate->xCoordinate.'-7, '.$coordinate->yCoordinate}}+5);
       }
     @endforeach
 
@@ -32,18 +36,58 @@
       @foreach($coordinates as $coordinate)
       if({{$coordinate->step}} == step){
         ctx.beginPath();
-        ctx.arc({{$coordinate->xCoordinate}}, {{$coordinate->yCoordinate}}, 2, 0, 2 * Math.PI);
+        ctx.arc({{$coordinate->xCoordinate.', '.$coordinate->yCoordinate}}, 10, 0, 2 * Math.PI);
         ctx.fillStyle = "#000000";
         ctx.fill();
+        ctx.fillStyle = "#DDDDDD";
+        ctx.font="12px Arial";
+        ctx.fillText("{{$coordinate->id}}",{{$coordinate->xCoordinate.'-6, '.$coordinate->yCoordinate}}+5);
       }
-      else if({{$coordinate->step}} == (step -1)){
+      else if({{$coordinate->step}} == (step-1)){
         ctx.beginPath();
-        ctx.arc({{$coordinate->xCoordinate}}, {{$coordinate->yCoordinate}}, 2, 0, 2 * Math.PI);
-        ctx.fillStyle = "#444444";
+        ctx.arc({{$coordinate->xCoordinate.', '.$coordinate->yCoordinate}}, 8, 0, 2 * Math.PI);
+        ctx.fillStyle = "#666666";
         ctx.fill();
+        ctx.fillStyle = "#FFFFFF";
+        ctx.font="8px Arial";
+        ctx.fillText("{{$coordinate->id}}",{{$coordinate->xCoordinate.'-4.5, '.$coordinate->yCoordinate}}+3.5);
       }
       @endforeach
     }
+
+    function runSteps(max){
+
+      let i = 1;
+      let counter = max;
+      let interval = setInterval(function(){
+        updateStep(i);
+        i++;
+        document.getElementById('step').value = i;
+        if(i == counter){
+          clearInterval(interval);
+          return updateStep(i);
+        }
+      },200);
+    }
+
+    function resetSteps(){
+      return updateStep(1);
+        document.getElementById('step').value = '1';
+    }
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+
+      canvas.addEventListener('click', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+        console.log(message);
+      }, false);
   </script>
 
 
